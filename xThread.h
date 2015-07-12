fd7e8b0e6e19641b47722bc173a1c44223fcc0a8
xiaoyifeibupt <xiaoyifeibupt@outlook.com>
2015-07-12 21:25:20 +0800

    pthread头文件封装

---
+++ xThread.h
@@ -0,0 +1,46 @@
+#ifndef XTHREAD_H
+#define XTHREAD_H
+
+#include <pthread.h>
+#include <functional>
+#include <memory>
+
+namespace xMultiThread {
+
+	class xThread {
+		public:
+			typedef std::function<void ()> ThreadFunc;
+
+			explicit xThread(const ThreadFunc&, const std::string& name = std::string());
+			
+			~xThread();
+
+			void start();
+			void join();
+
+			bool started() const { return started_; }
+
+			pid_t tid() const { return *tid_; }
+			
+			const std::string& name() const { return name_; }
+
+		private:
+			xThread(const xThread&) = delete;
+			xThread& operator=(const xThread&) = delete;
+			bool        started_;
+			bool        joined_;
+			pthread_t   pthreadId_;
+			std::shared_ptr<pid_t> tid_;
+			ThreadFunc  func_;
+			std::string name_;
+	};
+
+	namespace CurrentThread {
+		pid_t tid();
+		const char* name();
+		bool isMainThread();
+	}
+
+}
+
+#endif

dec6c5e3e62e0d21b7dfeac3deb50db85f530de3
xiaoyifeibupt <xiaoyifeibupt@outlook.com>
2015-07-13 16:29:30 +0800

    xMutex

---
+++ xMutex.h
@@ -0,0 +1,81 @@
+#ifndef XMUTEX_H
+#define XMUTEX_H
+
+#include "xThread.h"
+
+#include <assert.h>
+#include <pthread.h>
+
+namespace xMultiThread {
+
+	class xMutexLock {
+	public:
+		xMutexLock()
+			: holder_(0) {
+			pthread_mutex_init(&mutex_, NULL);
+		}
+
+		~xMutexLock() {
+			assert(holder_ == 0);
+			pthread_mutex_destroy(&mutex_);
+		}
+
+		bool isLockedByThisThread() {
+			return holder_ == CurrentThread::tid();
+		}
+
+		void assertLocked() {
+			assert(isLockedByThisThread());
+		}
+
+		// internal usage
+
+		void lock() {
+			pthread_mutex_lock(&mutex_);
+			holder_ = CurrentThread::tid();
+		}
+
+		void unlock() {
+			holder_ = 0;
+			pthread_mutex_unlock(&mutex_);
+		}
+
+		pthread_mutex_t* getPthreadMutex() {
+			return &mutex_;
+		}
+
+	private:
+
+		xMutexLock(const xMutexLock&) = delete;
+		xMutexLock& operator=(const xMutexLock&) = delete;
+
+		pthread_mutex_t mutex_;
+		pid_t holder_;
+	};
+
+	class xMutexLockGuard {
+	public:
+		explicit xMutexLockGuard(xMutexLock& mutex) : mutex_(mutex) {
+			mutex_.lock();
+		}
+
+		~xMutexLockGuard() {
+			mutex_.unlock();
+		}
+
+	private:
+
+		xMutexLockGuard(const xMutexLockGuard&) = delete;
+		xMutexLockGuard& operator=(const xMutexLockGuard&) = delete;
+
+		xMutexLock& mutex_;
+	};
+
+}
+
+// 防止以下错误使用
+// MutexLockGuard(mutex_);
+// 局部对象只能锁住它所在的那一行
+#define xMutexLockGuard(x) error "Missing guard object name"
+
+#endif

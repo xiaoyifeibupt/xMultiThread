14c9fd41465d906d95313a5928d34a7a7dd84bc4
xiaoyifeibupt <xiaoyifeibupt@outlook.com>
2015-07-15 20:19:53 +0800

    xThreadPool.h

---
+++ xThreadPool.h
@@ -0,0 +1,46 @@
+#ifndef XTHREADPOOL_H
+#define XTHREADPOOL_H
+
+#include "Condition.h"
+#include "Mutex.h"
+#include "Thread.h"
+
+#include <functional>
+#include <memory>
+
+#include <vector>
+#include <deque>
+
+namespace xMultiThread {
+
+	class ThreadPool {
+		public:
+			typedef std::function<void ()> Task;
+
+			explicit ThreadPool(const std::string& name = std::string());
+			~ThreadPool();
+
+			void start(int numThreads);
+			void stop();
+
+			void run(const Task& f);
+
+		private:
+
+			xThreadPool(const xThreadPool&) = delete;
+			xThreadPool& operator=(const xThreadPool&) = delete;
+
+			void runInThread();
+			Task take();
+
+			mutable MutexLock mutex_;
+			Condition cond_;
+			std::string name_;
+			std::vector<std::unique_ptr<xMultiThread::Thread> threads_;
+			std::deque<Task> queue_;
+			bool running_;
+	};
+
+}
+
+#endif
